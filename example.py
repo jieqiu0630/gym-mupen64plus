0bb0ad49c14777ca46d1cf8bea354d1142cdddc3
Jie Qiu <jieqiu0630@gmail.com>
2021-04-16 12:08:10 -0700

    Fix

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 #!/bin/python
 import gym, gym_mupen64plus
 
-env = gym.make('Mario-Kart-Luigi-Raceway-v0')
 env2 = gym.make('Mario-Kart-Luigi-Raceway-v0')
+env = gym.make('Mario-Kart-Luigi-Raceway-v0')
 env.reset()
 
 print("NOOP waiting for green light")
